26ef745ee235d83d9e3616cde9a55526f4dd14d2
hdqtonz <hiten.qtonz@yahoo.com>
2022-03-21 16:07:11 +0530

    Updating wall page

--- src/app/product/wall-photo/wall-photo.component.ts
+++ src/app/product/wall-photo/wall-photo.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wall-photo.component.css']
 })
 export class WallPhotoComponent implements OnInit {
-
+  zoom=false;
+  text=false;
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+// zoom and text input show and hide functions
+  showZoom(){
+    this.zoom = true
+    this.text = false
+  }
+  showText(){
+    this.zoom = false
+    this.text = true
+  }
 }
