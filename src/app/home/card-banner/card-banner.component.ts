e473e0a42932dc1b811caa47c41147296560ce42
hdqtonz <hiten.qtonz@yahoo.com>
2022-08-02 17:17:20 +0530

    changing layout design

--- src/app/home/card-banner/card-banner.component.ts
+++ src/app/home/card-banner/card-banner.component.ts
@@ -3,20 +3,43 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-card-banner',
   templateUrl: './card-banner.component.html',
-  styleUrls: ['./card-banner.component.css']
+  styleUrls: ['./card-banner.component.css'],
 })
 export class CardBannerComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   cards = [
-    { link:"http://qtonz.com", img:"../../../assets/images/home-page-images/home_page_card_banner_Image-1.jpeg", title:"Custom Acrylic Prints" },
-    { link:"http://qtonz.com", img:"../../../assets/images/home-page-images/home_page_card_banner_Image-2.gif", title:"Custom Phone Cases"},
-    { link:"http://qtonz.com", img:"../../../assets/images/home-page-images/home_page_card_banner_Image-3.webp", title:" Custom Posters"},
-    { link:"http://qtonz.com", img:"../../../assets/images/home-page-images/home_page_card_banner_Image-4.webp", title:"Custom Canvas"},
-  ]
-
+    {
+      link: 'http://qtonz.com',
+      img: '../../../assets/images/home-page-images/home_page_card_banner_Image-1.jpeg',
+      title: 'Custom Acrylic Prints',
+    },
+    {
+      link: 'http://qtonz.com',
+      img: '../../../assets/images/home-page-images/home_page_card_banner_Image-2.gif',
+      title: 'Custom Phone Cases',
+    },
+    {
+      link: 'http://qtonz.com',
+      img: '../../../assets/images/home-page-images/home_page_card_banner_Image-3.webp',
+      title: ' Custom Posters',
+    },
+    {
+      link: 'http://qtonz.com',
+      img: '../../../assets/images/home-page-images/home_page_card_banner_Image-2.gif',
+      title: 'Custom Canvas',
+    },
+    {
+      link: 'http://qtonz.com',
+      img: '../../../assets/images/home-page-images/home_page_card_banner_Image-4.webp',
+      title: 'Custom Canvas',
+    },
+    {
+      link: 'http://qtonz.com',
+      img: '../../../assets/images/home-page-images/home_page_card_banner_Image-1.jpeg',
+      title: 'Custom Canvas',
+    },
+  ];
 }
